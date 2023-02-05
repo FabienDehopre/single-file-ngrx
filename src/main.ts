2015ff06c1b85b842d7466cb05fcc342d432d740
Fabien Dehopré <fabien.dehopre@external.equans.com>
2023-02-05 11:46:39 +0100

    add material

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,14 @@ import {provideHttpClient} from "@angular/common/http";
 import {provideStore} from "@ngrx/store";
 import {provideStoreDevtools} from "@ngrx/store-devtools";
 import {provideEffects} from "@ngrx/effects";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent, { providers: [
-  provideRouter(APP_ROUTES),
+    provideRouter(APP_ROUTES),
     provideHttpClient(),
     provideStore(),
     provideStoreDevtools(),
-    provideEffects()
-  ] }).catch((err) => console.error(err));
+    provideEffects(),
+    importProvidersFrom(BrowserAnimationsModule)
+] }).catch((err) => console.error(err));
